5171c1153f60654534c115e892656590866a92be
Alexander Moisseev <moiseev@mezonplus.ru>
2017-11-19 13:44:48 +0300

    Fix test

--- md5.sql
+++ md5.sql
@@ -10,4 +10,4 @@ drop table t1;
 
 select 'ERROR' where hex(md5(''))!='D41D8CD98F00B204E9800998ECF8427E';
 select 'ERROR' where hex(md5_utf16('a'))!='4144E195F46DE78A3623DA7364D04F11';
-select 'ERROR' where hex(md5file('md5.c'))!='aca7c0e51f6dcda1419414105cded019' collate nocase;
+select 'ERROR' where hex(md5file('LICENSE'))!='911690f51af322440237a253d695d19f' collate nocase;
